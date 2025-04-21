239ebdff8ef4462ac5b2181e08e2794037382456
Luis Hernandez <luismiguel.hernandez@ulpgc.es>
2025-04-21 20:33:24 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flora-fauna-favorita-ionic-firebase',
+  webDir: 'www'
+};
+
+export default config;
